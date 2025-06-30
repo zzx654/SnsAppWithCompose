8461989a9046425615e82edfb7a4e2714f30b8f8
LeeMyoungHan <mhyhl654@gmail.com>
2025-06-30 18:21:41 +0900

    TabPager 리팩터링: 탭과 페이지를 매개변수로 받아 유연성 향상

--- presentation/src/main/java/com/androiddev/snsappwithcompose/components/TabPager.kt
+++ presentation/src/main/java/com/androiddev/snsappwithcompose/components/TabPager.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Tab
 import androidx.compose.material.Text
@@ -17,29 +18,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.androiddev.snsappwithcompose.home.nearposts.NearPostsScreen
 import kotlinx.coroutines.launch
 
 @Composable
 fun TabPager(
-    navController: NavController
-    //pagerState: PagerState,
-    //tabs: List<String>,
-    //Screens: List<@Composable (() -> Unit)>
+    tabs: List<String>,
+    pages: List<@Composable () -> Unit>,
+    pagerState: PagerState = rememberPagerState { tabs.size },
 ) {
     val coroutineScope = rememberCoroutineScope()
-
-    val tabs = listOf("근처", "인기", "새로운","팔로우","태그","사람")
-    val pagerState = rememberPagerState(
-        pageCount = { tabs.size },
-        initialPageOffsetFraction = 0f,
-        initialPage = 0,
-    )
     val tabIndex = pagerState.currentPage
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
+
+    Column(modifier = Modifier.fillMaxSize()) {
         SecondaryTabRow(
             selectedTabIndex = tabIndex,
             indicator = {
@@ -63,24 +53,19 @@ fun TabPager(
                         Text(text = "${tabs[index]}")
                         Spacer(modifier = Modifier.height(15.dp))
                     }
-
-
                 }
             }
         }
-        HorizontalPager(state = pagerState, userScrollEnabled = true) {
 
-            when(tabIndex) {
-                0 -> {
-                    NearPostsScreen(navController)
-                }
-                else -> {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = "${tabIndex}")
-                    }
+        HorizontalPager(state = pagerState) { page ->
+            if(page == 0)
+                pages[page]()
+            else {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "${tabIndex}")
                 }
             }
         }
--- presentation/src/main/java/com/androiddev/snsappwithcompose/home/HomeScreen.kt
+++ presentation/src/main/java/com/androiddev/snsappwithcompose/home/HomeScreen.kt
@@ -2,17 +2,41 @@ package com.androiddev.snsappwithcompose.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.androiddev.snsappwithcompose.components.TabPager
+import com.androiddev.snsappwithcompose.home.nearposts.NearPostsScreen
 
 @Composable
 fun HomeScreen(navController: NavController) {
-
+    val tabs = listOf("근처", "인기", "새로운","팔로우","태그","사람")
+    val pages = listOf<@Composable () -> Unit>(
+        {NearPostsScreen(navController)},
+        {PlaceholderScreen("인기")},
+        {PlaceholderScreen("새로운")},
+        {PlaceholderScreen("팔로우")},
+        {PlaceholderScreen("태그")},
+        {PlaceholderScreen("사람")}
+    )
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        TabPager(navController)
+        TabPager(
+            tabs = tabs,
+            pages = pages
+        )
+    }
+}
+@Composable
+fun PlaceholderScreen(title: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "$title 화면 준비중...", fontSize = 20.sp)
     }
 }
